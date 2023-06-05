a4865785e67865b474e5cd91a9ed0009742470ff
Kuba Wawrzyn <j.wawrzyn99@gmail.com>
2023-06-05 12:30:30 +0200

    finished login page layout

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,8 +1,36 @@
 import React from 'react';
 
-function Login(props) {
+function Login() {
     return (
-        <div>Tutaj będzie ekran logowania</div>
+        <div className="container">
+            <nav className="home__nav">
+                    <span className="home__nav-top">
+                        <a href="" className="home__account">Zaloguj</a>
+                        <a href="" className="home__account">Załóż konto</a>
+                    </span>
+                <span className="home__nav-bottom">
+                        <a href="" className="home__nav-item">Start</a>
+                        <a href="" className="home__nav-item">O co chodzi?</a>
+                        <a href="" className="home__nav-item">O nas</a>
+                        <a href="" className="home__nav-item">Fundacja i organizacje</a>
+                        <a href="" className="home__nav-item">Kontakt</a>
+                    </span>
+            </nav>
+            <section className="login__wrapper">
+                <h1 className="login__header">Zaloguj się</h1>
+                <img src="src/assets/Decoration.svg" alt="" className="login__decoration"/>
+                <form action="" className="login__form">
+                    <label htmlFor="login__email" className="login__label">Email</label>
+                    <input type="email" id="login__email" className="login__input"/>
+                    <label htmlFor="login__password" className="login__label">Hasło</label>
+                    <input type="password" id="login__password" className="login__input"/>
+                </form>
+                <span className="login__buttons">
+                        <button className="login__register-btn">Załóż konto</button>
+                        <button className="login__login-btn">Zaloguj się</button>
+                    </span>
+            </section>
+        </div>
     );
 }
 
